042d894790b3d8a50972dbc2407f28dd831fcb5d
Franky Liu <cuifranky95@gmail.com>
2019-08-21 00:59:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,8 +40,10 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  newvalue = true
   source_array.each do |index|
-    source_array.index
+    newvalue = !!source_array.index
   end
+  return newvalue
 end
     
