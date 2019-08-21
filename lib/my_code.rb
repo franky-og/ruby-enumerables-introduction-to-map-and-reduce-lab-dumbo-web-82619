2d2046b42b7896cee29b510021ba58e39c1db93f
Franky Liu <cuifranky95@gmail.com>
2019-08-21 00:18:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,7 @@
 # My Code here....
 def map_to_negativize(source_array)
   newarray = []
-  source_array.each |index|
+  source_array.each do |index|
     newarray.push(index * -1)
   end
   return newarray
